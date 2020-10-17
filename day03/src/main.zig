029c8e7878dafc367bdadbe4efe3cec1fa07ee1d
Travis <tssund93@gmail.com>
2020-10-17 00:46:33 -0500

    day 3

---
+++ day03/src/main.zig
@@ -0,0 +1,86 @@
+const std = @import("std");
+const util = @import("util.zig");
+const SplitIterator = std.mem.SplitIterator;
+
+const Triangle = struct { x: u16, y: u16, z: u16 };
+
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+    const input = try util.readFileIntoString(allocator, "input.txt", 1024 * 100);
+    defer allocator.free(input);
+
+    const partOneResult = partOne(input);
+    std.debug.print("Part 1: {}\n", .{partOneResult});
+
+    const partTwoResult = try partTwo(input);
+    std.debug.print("Part 2: {}\n", .{partTwoResult});
+}
+
+fn partOne(input: []u8) u16 {
+    var lines = std.mem.split(std.fmt.trim(input), "\n");
+    var triangles: u16 = 0;
+
+    while (lines.next()) |line| {
+        const x = std.fmt.trim(line[0..5]);
+        const y = std.fmt.trim(line[5..10]);
+        const z = std.fmt.trim(line[10..]);
+        const triangle = .{
+            .x = parseSide(x),
+            .y = parseSide(y),
+            .z = parseSide(z),
+        };
+        if (isTriangle(triangle)) triangles += 1;
+    }
+
+    return triangles;
+}
+
+fn partTwo(input: []u8) !u16 {
+    var lines = std.mem.split(std.fmt.trim(input), "\n");
+    var triangles: u16 = 0;
+    const sides = 3;
+
+    while (lines.rest().len > 0) {
+        var x: [sides][]const u8 = undefined;
+        var y: [sides][]const u8 = undefined;
+        var z: [sides][]const u8 = undefined;
+        var i: u2 = 0;
+        try populateSides(&x, &lines);
+        try populateSides(&y, &lines);
+        try populateSides(&z, &lines);
+        i = 0;
+        while (i < sides) : (i += 1) {
+            const triangle = .{
+                .x = parseSide(x[i]),
+                .y = parseSide(y[i]),
+                .z = parseSide(z[i]),
+            };
+            if (isTriangle(triangle)) triangles += 1;
+        }
+    }
+
+    return triangles;
+}
+
+fn isTriangle(triangle: Triangle) bool {
+    if (triangle.x + triangle.y > triangle.z and
+        triangle.y + triangle.z > triangle.x and
+        triangle.x + triangle.z > triangle.y)
+    {
+        return true;
+    }
+    return false;
+}
+
+fn populateSides(sides: *[3][]const u8, lines: *SplitIterator) !void {
+    const line = lines.next() orelse return error.WrongNumberOfLines;
+    sides[0] = std.fmt.trim(line[0..5]);
+    sides[1] = std.fmt.trim(line[5..10]);
+    sides[2] = std.fmt.trim(line[10..]);
+}
+
+fn parseSide(side: []const u8) u16 {
+    return std.fmt.parseUnsigned(u16, side, 10) catch |err| {
+        util.exitWithError(err, "couldn't parse '{}'", .{side});
+    };
+}
