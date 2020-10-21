a3e98039ed5451a4502be29a4b2618cf5d533b23
Travis <tssund93@gmail.com>
2020-10-20 23:27:13 -0500

    removed duplication in day 5

--- day05/main.zig
+++ day05/main.zig
@@ -8,13 +8,35 @@ pub fn main() anyerror!void {
     const input = "ugkcyxxp";
     var output: [Md5.digest_length]u8 = undefined;
     var password: [8]u8 = undefined;
-    try crackPasswordPart1(input, &password);
+    try crackPassword(input, &password, buildPasswordPart1);
     std.debug.print("Part 1: {}\n", .{password});
-    try crackPasswordPart2(input, &password);
+    try crackPassword(input, &password, buildPasswordPart2);
     std.debug.print("Part 2: {}\n", .{password});
 }
 
-fn crackPasswordPart1(input: []const u8, password: *[8]u8) !void {
+fn buildPasswordPart1(password: *[8]u8, hash: []const u8, index: *u4) void {
+    password[index.*] = hash[5];
+    index.* += 1;
+}
+
+fn buildPasswordPart2(password: *[8]u8, hash: []const u8, index: *u4) void {
+    const position = fmt.parseUnsigned(u4, hash[5..6], 10) catch null;
+    if (position) |pos| {
+        if (pos < 8 and password[pos] == 0) {
+            password[pos] = hash[6];
+            index.* += 1;
+        }
+    }
+}
+
+fn crackPassword(
+    input: []const u8,
+    password: *[8]u8,
+    buildPasswordFn: fn (password: *[8]u8, hash: []const u8, index: *u4) void,
+) !void {
+    for (password) |*c| {
+        c.* = 0;
+    }
     var buf: [100]u8 = undefined;
     var output: [Md5.digest_length]u8 = undefined;
     var password_index: u4 = 0;
@@ -26,8 +48,7 @@ fn crackPasswordPart1(input: []const u8, password: *[8]u8) !void {
         }
         const hash = try fmt.bufPrint(&buf, "{x}", .{output});
         if (mem.eql(u8, hash[0..5], "00000")) {
-            password[password_index] = hash[5];
-            password_index += 1;
+            buildPasswordFn(password, hash, &password_index);
         }
     }
 }
@@ -35,39 +56,13 @@ fn crackPasswordPart1(input: []const u8, password: *[8]u8) !void {
 test "crackPasswordPart1" {
     const input = "abc";
     var password: [8]u8 = undefined;
-    try crackPasswordPart1(input, &password);
+    try crackPassword(input, &password, buildPasswordPart1);
     testing.expectEqualStrings("18f47a30", &password);
-}
-
-fn crackPasswordPart2(input: []const u8, password: *[8]u8) !void {
-    for (password) |*c| {
-        c.* = 0;
-    }
-    var buf: [100]u8 = undefined;
-    var output: [Md5.digest_length]u8 = undefined;
-    var password_fill_count: u4 = 0;
-    var i: usize = 0;
-    while (password_fill_count < 8) : (i += 1) {
-        {
-            const result = try fmt.bufPrint(&buf, "{}{}", .{ input, i });
-            Md5.hash(result, &output, .{});
-        }
-        const hash = try fmt.bufPrint(&buf, "{x}", .{output});
-        if (mem.eql(u8, hash[0..5], "00000")) {
-            const position = fmt.parseUnsigned(u4, hash[5..6], 10) catch null;
-            if (position) |pos| {
-                if (pos < 8 and password[pos] == 0) {
-                    password[pos] = hash[6];
-                    password_fill_count += 1;
-                }
-            }
-        }
-    }
 }
 
 test "crackPasswordPart2" {
     const input = "abc";
     var password: [8]u8 = undefined;
-    try crackPasswordPart2(input, &password);
+    try crackPassword(input, &password, buildPasswordPart2);
     testing.expectEqualStrings("05ace8e3", &password);
 }
