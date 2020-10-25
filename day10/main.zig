6681e77f19fb056f7ba77eb25fcdc1a309bfa1a4
Travis <tssund93@gmail.com>
2020-10-25 16:29:53 -0500

    removed debug statements from day 10

--- day10/main.zig
+++ day10/main.zig
@@ -129,13 +129,8 @@ const Bot = struct {
 
     fn receive(self: *Self, state: *State, chip_num: u16) !?u16 {
         assert(self.microchips.items.len < 2);
-        log.debug("bot {} receiving microchip {}", .{ self.number, chip_num });
         try self.microchips.append(chip_num);
 
-        log.debug("bot {} microchips:", .{self.number});
-        for (self.microchips.items) |chip| {
-            log.debug("{}", .{chip});
-        }
         if (self.microchips.items.len == 2) {
             return try self.carryOutInstruction(state);
         }
@@ -160,31 +155,21 @@ const Bot = struct {
         }
 
         const instruction = state.instructions.get(self.number) orelse return error.InstructionNotFound;
-        log.debug("bot {} microchip length before popping low: {}", .{ self.number, self.microchips.items.len });
         const low_microchip = self.microchips.pop();
         switch (instruction.low_to.type) {
             .bot => {
                 var bot = try registerBot(self.allocator, &state.bots, instruction.low_to.num, self);
-                log.debug("bot {} ({}) giving microchip {} to bot {}", .{ self.number, &self, low_microchip, bot.number });
                 const result = try bot.receive(state, low_microchip);
-                log.debug("{}: bot {}", .{ &self, self.number });
                 if (result != null) return result;
-                log.debug("{}: bot {}", .{ &self, self.number });
             },
             .output => {
                 _ = try state.outputs.put(instruction.low_to.num, low_microchip);
             },
         }
-        log.debug("{}: bot {}", .{ &self, self.number });
-        log.debug("bot {} microchip length before popping high: {}", .{ self.number, self.microchips.items.len });
-        log.debug("{}: bot {}", .{ &self, self.number });
         const high_microchip = self.microchips.pop();
         switch (instruction.high_to.type) {
             .bot => {
-                log.debug("{}: bot {}", .{ &self, self.number });
                 var bot = try registerBot(self.allocator, &state.bots, instruction.high_to.num, self);
-                log.debug("{}: bot {}", .{ &self, self.number });
-                log.debug("bot {} ({}) giving microchip {} to bot {}", .{ self.number, &self, high_microchip, bot.number });
                 const result = try bot.receive(state, high_microchip);
                 if (result != null) return result;
             },
@@ -200,12 +185,8 @@ const Bot = struct {
 const CarryOutError = error{InstructionNotFound} || mem.Allocator.Error;
 
 fn registerBot(allocator: *Allocator, bots: *AutoHashMap(u16, Bot), bot_num: u16, old_bot: ?*Bot) !*Bot {
-    log.debug("creating bot {}", .{bot_num});
-    if (old_bot) |ob| log.debug("{}", .{ob.number});
     const entry = try bots.getOrPutValue(bot_num, try Bot.init(allocator, bot_num));
-    if (old_bot) |ob| log.debug("{}", .{ob.number});
     const bot = &entry.value;
-    if (old_bot) |ob| log.debug("{}", .{ob.number});
     errdefer bot.deinit();
     return bot;
 }
