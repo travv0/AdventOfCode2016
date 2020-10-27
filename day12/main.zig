d8915566bb6b59231208ee1bc34a50fe5de8e278
Travis <tssund93@gmail.com>
2020-10-26 21:39:20 -0500

    finished day  12

--- day12/main.zig
+++ day12/main.zig
@@ -7,11 +7,25 @@ const fmt = std.fmt;
 const mem = std.mem;
 const expectEqual = std.testing.expectEqual;
 
-pub fn main() anyerror!void {}
+pub fn main() anyerror!void {
+    const allocator = std.heap.page_allocator;
+    const input = try util.readInput(allocator, 1024 * 1024);
+    const part1_result = try runProgram(allocator, input, false);
+    std.debug.print("Part 1: {}\n", .{part1_result});
+    const part2_result = try runProgram(allocator, input, true);
+    std.debug.print("Part 2: {}\n", .{part2_result});
+}
 
 const Value = union(enum) {
     register: u8,
     integer: isize,
+
+    fn resolve(self: @This(), registers: AutoHashMap(u8, isize)) !isize {
+        return switch (self) {
+            .integer => |i| i,
+            .register => |reg| registers.get(reg) orelse error.RegisterEmpty,
+        };
+    }
 };
 
 const Cpy = struct {
@@ -31,12 +45,67 @@ const Command = union(enum) {
     jnz: Jnz,
 };
 
-fn handleCommand(commands: []*Command, i: usize) void {}
+fn handleCommand(commands: []Command, registers: *AutoHashMap(u8, isize), i: usize) !usize {
+    const command = commands[i];
+    switch (command) {
+        .cpy => |cpy| {
+            try registers.put(cpy.to, try cpy.from.resolve(registers.*));
+        },
+        .inc => |reg| {
+            const entry = registers.getEntry(reg) orelse return error.RegisterEmpty;
+            entry.value += 1;
+        },
+        .dec => |reg| {
+            const entry = registers.getEntry(reg) orelse return error.RegisterEmpty;
+            entry.value -= 1;
+        },
+        .jnz => |jnz| {
+            if ((try jnz.check.resolve(registers.*)) != 0) {
+                return @intCast(usize, @intCast(isize, i) + jnz.jump);
+            }
+        },
+    }
+    return i + 1;
+}
+
+test "runProgram" {
+    const allocator = std.testing.allocator;
+    const input =
+        \\cpy 41 a
+        \\inc a
+        \\inc a
+        \\dec a
+        \\jnz a 2
+        \\dec a
+    ;
+
+    const result = try runProgram(allocator, input, false);
+    expectEqual(@as(isize, 42), result);
+}
+
+fn runProgram(allocator: *Allocator, input: []const u8, ignite: bool) !isize {
+    const commands = try parseCommands(allocator, input);
+    defer allocator.free(commands);
+
+    var registers = AutoHashMap(u8, isize).init(allocator);
+    defer registers.deinit();
+
+    try registers.put('a', 0);
+    try registers.put('b', 0);
+    try registers.put('c', if (ignite) 1 else 0);
+    try registers.put('d', 0);
+
+    var i: usize = 0;
+    while (i < commands.len) {
+        i = try handleCommand(commands, &registers, i);
+    }
+    return registers.get('a') orelse error.RegisterEmpty;
+}
 
 fn parseCommands(allocator: *Allocator, input: []const u8) ![]Command {
     var commands = ArrayList(Command).init(allocator);
     errdefer commands.deinit();
-    var lines = mem.split(input, "\n");
+    var lines = mem.split(fmt.trim(input), "\n");
     while (lines.next()) |line| {
         const words = try util.split(allocator, line, " ");
         defer allocator.free(words);
@@ -44,7 +113,7 @@ fn parseCommands(allocator: *Allocator, input: []const u8) ![]Command {
         if (mem.eql(u8, words[0], "cpy")) {
             command = .{
                 .cpy = .{
-                    .from = if (fmt.parseUnsigned(isize, words[1], 10)) |int|
+                    .from = if (fmt.parseInt(isize, words[1], 10)) |int|
                         .{ .integer = int }
                     else |_|
                         .{ .register = words[1][0] },
@@ -58,11 +127,11 @@ fn parseCommands(allocator: *Allocator, input: []const u8) ![]Command {
         } else if (mem.eql(u8, words[0], "jnz")) {
             command = .{
                 .jnz = .{
-                    .check = if (fmt.parseUnsigned(isize, words[1], 10)) |int|
+                    .check = if (fmt.parseInt(isize, words[1], 10)) |int|
                         .{ .integer = int }
                     else |_|
                         .{ .register = words[1][0] },
-                    .jump = try fmt.parseUnsigned(isize, words[2], 10),
+                    .jump = try fmt.parseInt(isize, words[2], 10),
                 },
             };
         } else {
